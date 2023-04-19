93d865164c043feda767c9f99444d7f4d74c5204
martinyis <martinyis11@gmail.com>
2023-04-19 14:42:17 -0400

    Install express

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+import express from "express";
 
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello Worsdfsdfsd!");
+});
+
+app.listen(4444, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+  return console.log(`server is listening on 4444`);
+});
